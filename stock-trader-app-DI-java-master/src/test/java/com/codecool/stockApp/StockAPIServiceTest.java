6e95e49289de8cf577d3e6ca888928fd646f1460
MateMiklos <miklos.mate.91@gmail.com>
2019-09-20 15:14:40 +0200

    tests have been written

--- stock-trader-app-DI-java-master/src/test/java/com/codecool/stockApp/StockAPIServiceTest.java
+++ stock-trader-app-DI-java-master/src/test/java/com/codecool/stockApp/StockAPIServiceTest.java
@@ -1,25 +1,41 @@
 package com.codecool.stockApp;
 
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 // TODO
 class StockAPIServiceTest {
 
-    @Test // everything works
-    void testGetPriceNormalValues() {
+    private static final String url = String.format("https://financialmodelingprep.com/api/v3/stock/real-time-price/%s", "aapl");
 
+    @Test // everything works
+    void testGetPriceNormalValues() throws IOException {
+        RemoteURLReader remoteURLReader = mock(RemoteURLReader.class);
+        StockAPIService stockAPIService = new StockAPIService(remoteURLReader);
+        when(remoteURLReader.readFromUrl(url)).thenReturn("{\"price\":20}");
+        assertEquals(20.0, stockAPIService.getPrice("aapl"));
     }
 
     @Test // readFromURL threw an exception
-    void testGetPriceServerDown() {
-
+    void testGetPriceServerDown() throws IOException {
+        RemoteURLReader remoteURLReader = mock(RemoteURLReader.class);
+        StockAPIService stockAPIService = new StockAPIService(remoteURLReader);
+        when(remoteURLReader.readFromUrl(url)).thenThrow(IOException.class);
+        assertThrows(IOException.class, () -> stockAPIService.getPrice("aapl"));
     }
 
     @Test // readFromURL returned wrong JSON
-    void testGetPriceMalformedResponse() {
-
+    void testGetPriceMalformedResponse() throws IOException {
+        RemoteURLReader remoteURLReader = mock(RemoteURLReader.class);
+        StockAPIService stockAPIService = new StockAPIService(remoteURLReader);
+        when(remoteURLReader.readFromUrl(url)).thenReturn("{\"rice\":42}");
+        assertThrows(JSONException.class, () -> stockAPIService.getPrice("aapl"));
     }
 
 }
--- stock-trader-app-DI-java-master/src/test/java/com/codecool/stockApp/TraderTest.java
+++ stock-trader-app-DI-java-master/src/test/java/com/codecool/stockApp/TraderTest.java
@@ -1,20 +1,35 @@
 package com.codecool.stockApp;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import javax.swing.*;
+
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 // TODO
 class TraderTest {
 
 
     @Test // Bid was lower than price, buy should return false.
-    void testBidLowerThanPrice(){
+    void testBidLowerThanPrice() throws IOException {
+        StockAPIService stockAPIService = mock(StockAPIService.class);
+        Logger logger = mock(Logger.class);
+        Trader trader = new Trader(logger, stockAPIService);
+        when(stockAPIService.getPrice("aapl")).thenReturn(300.0);
+        assertFalse(trader.buy("aapl", 250.0));
     }
 
     @Test // bid was equal or higher than price, buy() should return true.
-    void testBidHigherThanPrice() {
-
+    void testBidHigherThanPrice() throws IOException {
+        StockAPIService stockAPIService = mock(StockAPIService.class);
+        Logger logger = mock(Logger.class);
+        Trader trader = new Trader(logger, stockAPIService);
+        when(stockAPIService.getPrice("aapl")).thenReturn(300.0);
+        assertTrue(trader.buy("aapl", 350.0));
     }
 }
